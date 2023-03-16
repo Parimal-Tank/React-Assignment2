b79b87c87ebf351251257c99e69c58eb1f1bd1ec
Parimal-Tank <parimalt@zignuts.com>
2023-03-16 16:47:13 +0530

    sort functionality completed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Gamesdetails from "./Gamesdetails";
-import axios from "axios";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const Search = () => {
   const [data, setData] = useState([{}]);
@@ -14,12 +18,19 @@ const Search = () => {
 
   const [suggestions, setSuggestions] = useState([]);
 
+  const [platform, setPlateForm] = useState([]);
+
+  const [dropdownlist, setDropdownList] = useState("");
+
   const loadData = async () => {
+
     const result = await axios(
       "https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json"
     );
+
     result.data.splice(0, 1);
     setData(result.data);
+    sortByPlateform(result.data);
     setSearchData(result.data);
   };
 
@@ -27,6 +38,7 @@ const Search = () => {
     loadData();
   }, []);
 
+  
   const onSuggestHandler = (text) => {
     setText(text);
 
@@ -42,6 +54,41 @@ const Search = () => {
     setSuggestions([]);
   };
 
+
+  const handleChange = (e) => {
+    setDropdownList(e.target.value);
+
+    if (e.target.value === "All Platform") {
+      setSearchData(data);
+    } else {
+      const sortedData = data.filter(
+        (f) => f.platform.toLowerCase() === e.target.value.toLowerCase()
+      );
+      setSearchData(sortedData);
+    }
+  };
+
+  // Sort Data by Platform
+  const sortByPlateform = (allData) => {
+    const allPlatform = [];
+
+    allData.map((f) => {
+      allPlatform.push(f.platform);
+    });
+
+    const uniquePlatforms = new Set();
+
+    allPlatform.map((ele) => {
+      uniquePlatforms.add(ele);
+    });
+
+    const uniquePlatformArray = [...uniquePlatforms];
+
+    uniquePlatformArray.push("All Platform");
+    setPlateForm(uniquePlatformArray);
+  };
+
+
   const inputHandler = (e) => {
     //convert input text to lower case and check(for case sensitive scenario)
     const lowerCase = e.target.value.toLowerCase();
@@ -55,7 +102,6 @@ const Search = () => {
     setInputText(lowerCase);
 
     if (lowerCase !== "") {
-
       // filter the search data from the data
       const result = data.filter((f) =>
         f.title.toLowerCase().includes(lowerCase)
@@ -65,9 +111,9 @@ const Search = () => {
         const storeSuggestion = [];
 
         // store suggestion data based on search
-        result.map((suggestionData, index) => (
+        result.map((suggestionData, index) =>
           storeSuggestion.push(result[index].title)
-        ));
+        );
 
         setSuggestions(storeSuggestion);
       }
@@ -76,8 +122,8 @@ const Search = () => {
 
   return (
     <div>
-      <div className="main">
-        <h1>Games Search</h1>
+      <div className="main container">
+        <h1 className="text-center">Games Search</h1>
         <div className="container search">
           <TextField
             id="outlined-basic"
@@ -101,6 +147,30 @@ const Search = () => {
                 );
               })}
           </div>
+        </div>
+
+        <div className="platform-dropdown">
+          <FormControl className="dropdown-value">
+            <InputLabel id="demo-simple-select-label">Platform</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={dropdownlist}
+              label="Platform"
+              onChange={handleChange}
+            >
+              {platform &&
+                platform.map((element) => {
+                  console.log("element: ", element);
+
+                  return (
+                    <MenuItem key={element} value={element}>
+                      {element}
+                    </MenuItem>
+                  );
+                })}
+            </Select>
+          </FormControl>
         </div>
 
         <Gamesdetails data={searchData} />
